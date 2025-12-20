8de65e2aa927b505d31c3296735b6a2be63690ba
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-12-20 17:49:40 +0200

    modified about section

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -10,7 +10,7 @@ export const personalInfo = {
   leetcode: "https://leetcode.com/u/mustafaahmed1997",
 };
 
-export const aboutText = `Hello! I'm Mustafa Ahmed. I'm a fresh graduated MERN stack developer using React.js, Next.js & Node.js. Capable of learning more about my field through my professional life in addition to learning new skills. I'm skilled in using HTML, CSS, JS, and creating RESTful applications in order to deliver an immersive and engaging user experience through efficient web development. Seeking a position in a stable company where I can use my skills to benefit the company.`;
+export const aboutText = `Highly motivated MERN stack developer with hands-on experience building full-stack web applications using React, Node.js, Express, MySQL and MongoDB. Skilled in designing scalable RESTful APIs, implementing SSR with Next.js, and managing state using Redux Toolkit. Adept at developing reusable component libraries, integrating cloud services (Supabase, Redis, Stripe, Pinata, Cloudinary, AWS services), and optimizing performance through clean, modular architectures. Passionate about problem-solving, continuous learning, and delivering production-grade, maintainable code.`;
 
 export const education = {
   degree: "Faculty of Engineering, Mechanical Department",
@@ -64,7 +64,15 @@ export const experienceData = [
 ];
 
 export const skillsData = {
-  Frontend: ["HTML", "CSS", "JavaScript", "TypeScript", "React.js", "Next.js", "TailwindCSS"],
+  Frontend: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React.js",
+    "Next.js",
+    "TailwindCSS",
+  ],
   Backend: ["Node.js", "Express.js", "RESTful APIs", "Prisma ORM", "Mongoose"],
   Databases: ["MongoDB", "MySQL", "Redis", "Supabase"],
   "DevOps & Tools": ["Docker", "Git/GitHub", "Webpack", "AWS EC2", "RabbitMQ"],
@@ -75,120 +83,171 @@ export const projectsData = [
   {
     id: "next1",
     name: "E-Commerce Admin Dashboard",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1753560353/Projects/Admin_dashboard_pswsyp.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1753560353/Projects/Admin_dashboard_pswsyp.png",
     category: "Next.js",
-    liveLink: "https://ecommerce-admin-dashboard-mustafa.vercel.app/login?redirectto=%2F",
+    liveLink:
+      "https://ecommerce-admin-dashboard-mustafa.vercel.app/login?redirectto=%2F",
     githubLink: "https://github.com/mustafaahmed-f/ecommerce_admin_dashboard",
-    description: "A modern and modular admin dashboard for managing e-commerce operations — including products, coupons, users, and more.",
+    description:
+      "A modern and modular admin dashboard for managing e-commerce operations — including products, coupons, users, and more.",
     technologies: ["Next.js", "TypeScript", "MongoDB", "Redis", "TailwindCSS"],
   },
   {
     id: "next2",
     name: "E-Commerce Platform",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1747771299/Projects/Ecommerce_nextjs_sdb2m0.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1747771299/Projects/Ecommerce_nextjs_sdb2m0.png",
     category: "Next.js",
     liveLink: "https://ecommerce-nextjs-by-mustafa.vercel.app/",
     githubLink: "https://github.com/mustafaahmed-f/ecommerce-nextjs",
-    description: "A modern e-commerce platform built with Next.js, featuring advanced features like offline cart support and autocomplete search.",
+    description:
+      "A modern e-commerce platform built with Next.js, featuring advanced features like offline cart support and autocomplete search.",
     technologies: ["Next.js", "TypeScript", "MongoDB", "Redis", "TailwindCSS"],
   },
   {
     id: "next3",
     name: "Blog Application",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021748/Projects/Trello_ax1kwm.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021748/Projects/Trello_ax1kwm.png",
     category: "Next.js",
     liveLink: "https://blog-next-app-by-mustafa.vercel.app/",
     githubLink: "https://github.com/mustafaahmed-f/blog-next-app",
-    description: "A modern, full-featured blog application demonstrating best practices in authentication, form handling, and performance optimization.",
+    description:
+      "A modern, full-featured blog application demonstrating best practices in authentication, form handling, and performance optimization.",
     technologies: ["Next.js", "TypeScript", "TailwindCSS"],
   },
   {
     id: "node1",
     name: "eCommerce Backend",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021749/Projects/eCommerce_kfdqqs.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021749/Projects/eCommerce_kfdqqs.png",
     category: "Node.js",
     githubLink: "https://github.com/mustafaahmed-f/E-Commerce-back-end",
-    description: "Designed and developed more than 40 RESTful APIs for e-commerce platform with secure user authentication using JWT.",
-    technologies: ["Node.js", "JWT", "Stripe", "Nodemailer", "Multer", "MongoDB"],
+    description:
+      "Designed and developed more than 40 RESTful APIs for e-commerce platform with secure user authentication using JWT.",
+    technologies: [
+      "Node.js",
+      "JWT",
+      "Stripe",
+      "Nodemailer",
+      "Multer",
+      "MongoDB",
+    ],
   },
   {
     id: "node2",
     name: "SARAHA Backend",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021748/Projects/SARAHA_euojtu.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021748/Projects/SARAHA_euojtu.png",
     category: "Node.js",
     githubLink: "https://github.com/mustafaahmed-f/Saraha_Backend",
-    description: "Anonymous messaging platform where users can send and receive messages without revealing identities.",
+    description:
+      "Anonymous messaging platform where users can send and receive messages without revealing identities.",
     technologies: ["Node.js", "JWT", "Nodemailer", "Multer", "MongoDB"],
   },
   {
     id: "node3",
     name: "Blog-app Backend",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021748/Projects/Trello_ax1kwm.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021748/Projects/Trello_ax1kwm.png",
     category: "Node.js",
     githubLink: "https://github.com/mustafaahmed-f/blog-app-nodejs",
-    description: "A robust and scalable blog backend with modern API for blog management, authentication, and media handling.",
-    technologies: ["Node.js", "Prisma", "MySQL", "Redis", "AWS EC2", "AWS API Gateway"],
+    description:
+      "A robust and scalable blog backend with modern API for blog management, authentication, and media handling.",
+    technologies: [
+      "Node.js",
+      "Prisma",
+      "MySQL",
+      "Redis",
+      "AWS EC2",
+      "AWS API Gateway",
+    ],
   },
   {
     id: "react1",
     name: "Saraha Frontend",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021711/Projects/Saraha_wb3ccg.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021711/Projects/Saraha_wb3ccg.png",
     category: "React",
     liveLink: "https://saraha-by-mustafa.netlify.app/",
     githubLink: "https://github.com/mustafaahmed-f/SARAHA_Front-end",
-    description: "User authentication system with messaging features, responsive UI, and pagination for displaying messages.",
+    description:
+      "User authentication system with messaging features, responsive UI, and pagination for displaying messages.",
     technologies: ["React.js", "Redux", "i18next"],
   },
   {
     id: "react2",
     name: "Movies App",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021735/Projects/Movies_app_c993cp.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021735/Projects/Movies_app_c993cp.png",
     category: "React",
     liveLink: "https://moviesappbymustafa.netlify.app/",
     githubLink: "https://github.com/mustafaahmed-f/movies_project",
-    description: "An intuitive interface for searching movies using the TMDb API to explore a vast collection of movies.",
+    description:
+      "An intuitive interface for searching movies using the TMDb API to explore a vast collection of movies.",
     technologies: ["React.js", "Redux", "TypeScript", "React Query"],
   },
   {
     id: "react3",
     name: "Tello (Trello Clone)",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1730406206/Projects/Trello_asyoaw.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1730406206/Projects/Trello_asyoaw.png",
     category: "React",
     liveLink: "https://trellobymustafa.netlify.app/",
     githubLink: "https://github.com/mustafaahmed-f/trello",
-    description: "A web-based application for managing tasks and projects with drag and drop functionality.",
-    technologies: ["React.js", "TypeScript", "Supabase", "TailwindCSS", "DnD Library"],
+    description:
+      "A web-based application for managing tasks and projects with drag and drop functionality.",
+    technologies: [
+      "React.js",
+      "TypeScript",
+      "Supabase",
+      "TailwindCSS",
+      "DnD Library",
+    ],
   },
   {
     id: "vanilla1",
     name: "Weather Forecast",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021719/Projects/weather_forecast_i1httc.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021719/Projects/weather_forecast_i1httc.png",
     category: "Vanilla JS",
     liveLink: "https://mustafaahmed-f.github.io/weatherForecastSimpleProject/",
-    githubLink: "https://github.com/mustafaahmed-f/weatherForecastSimpleProject",
+    githubLink:
+      "https://github.com/mustafaahmed-f/weatherForecastSimpleProject",
     description: "Simple web application to get weather of different cities.",
     technologies: ["JavaScript", "HTML", "CSS"],
   },
   {
     id: "vanilla2",
     name: "Media Player",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021718/Projects/Media_player_amgni7.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021718/Projects/Media_player_amgni7.png",
     category: "Vanilla JS",
     liveLink: "https://mustafaahmed-f.github.io/MediaPlayer/",
     githubLink: "https://github.com/mustafaahmed-f/MediaPlayer",
-    description: "A simple media player where you can choose a SURAH from a drop list and listen to it.",
+    description:
+      "A simple media player where you can choose a SURAH from a drop list and listen to it.",
     technologies: ["JavaScript", "HTML", "CSS"],
   },
   {
     id: "vanilla3",
     name: "Food App",
-    imgUrl: "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021739/Projects/Food_website_a7lncr.png",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1722021739/Projects/Food_website_a7lncr.png",
     category: "Vanilla JS",
     liveLink: "https://mustafaahmed-f.github.io/yummi-new/",
     githubLink: "https://github.com/mustafaahmed-f/yummi-new",
-    description: "A website about foods all over the world where you can search and know details about recipes.",
+    description:
+      "A website about foods all over the world where you can search and know details about recipes.",
     technologies: ["JavaScript", "HTML", "CSS"],
   },
 ];
 
-export const projectCategories = ["All", "Next.js", "React", "Node.js", "Vanilla JS"];
+export const projectCategories = [
+  "All",
+  "Next.js",
+  "React",
+  "Node.js",
+  "Vanilla JS",
+];
